e4df6bab54cc7302e3d523c7db2f87f680d17f38
John Kristan Torremocha <johnkristan01@gmail.com>
2024-11-09 10:06:33 +0800

    feat: implemented local and jwt auth guard

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,13 +1,50 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Logger, Post, Request, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDTO } from 'src/users/dto/user';
+import { LocalAuthGuard } from './guard/local-auth.guard';
+import { JwtAuthGuard } from './guard/jwt-auth.guard';
+
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService){}
 
+    private logger = new Logger(AuthController.name, { timestamp: true })
+
+    @HttpCode(HttpStatus.CREATED)
     @Post('register')
-    register(@Body() registerDTO: RegisterUserDTO){
+    register(
+        @Body() registerDTO: RegisterUserDTO
+    ){
         return this.authService.register(registerDTO.name, registerDTO.email, registerDTO.password)
+    }
+
+    @UseGuards(LocalAuthGuard)
+    @HttpCode(HttpStatus.OK)
+    @Post('signin')
+    async signIn(
+        @Request() req, 
+        @Res({ passthrough: true }) res: Response
+    ): Promise<{ message: string; }> {
+        this.logger.debug(`Request Payload: ${req.user}`)
+        const jwtToken = await this.authService.signedJwt(req.user);
+
+        res.cookie('accessToken', jwtToken, {
+            httpOnly: true,  
+            secure: process.env.NODE_ENV === 'PRODUCTION', 
+            maxAge: 3600000, 
+        });
+
+        return { message: 'Login successful' };
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('profile')
+    getProfile(
+        @Request() req
+    ) {
+        this.logger.debug(`Request Payload: ${req.user}`)
+        return req.user;
     }
 }
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,14 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 
-import { hash, genSalt } from 'bcrypt';
+import { hash, genSalt, compare } from 'bcrypt';
 import { User } from 'src/users/entities/user/user';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
     constructor(
         private usersServices: UsersService,
+        private jwtService: JwtService
     ){}
+
+    private logger = new Logger(AuthService.name, { timestamp: true });
 
     async register(name: string, email: string, password: string): Promise<User> {
         
@@ -17,4 +21,26 @@ export class AuthService {
         const hashedPassword = await hash(password, saltRounds);
         return this.usersServices.createUser(name, email, hashedPassword);
     }
+
+    async validateUser(email: string, password: string): Promise<User | null> {
+        const user = await this.usersServices.findUserByEmail(email);
+
+        if(user){
+            const isMatch = await compare(password, user.password);    
+            if(isMatch) return user;   
+        }
+             
+        return null;
+    }
+
+    async signedJwt(user: User): Promise<string> {
+        const payload = { 
+            email: user.email,
+            name: user.name, 
+            sub: user.id 
+        };
+
+        return this.jwtService.sign(payload)
+    }
+
 }
