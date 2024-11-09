779818dd35e6dc1888f00d45a94395c13004499b
John Kristan Torremocha <johnkristan01@gmail.com>
2024-11-09 10:14:37 +0800

    feat: setup strategy for local and jwt authentication passport

---
+++ server/src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,32 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { jwtConstants } from '../constants';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(){
+        super({
+
+            jwtFromRequest: ExtractJwt.fromExtractors([(req) => {
+                if (req && req.cookies) {
+                    return req.cookies['accessToken']; 
+                }
+                return null;
+            }]),
+
+            ignoreExpiration: false,
+            secretOrKey: jwtConstants.secret,
+        })
+    }
+
+    async validate(payload: any) {
+        const authenticatedUser = { 
+            id: payload.sub, 
+            name: payload.name, 
+            email: payload.email 
+        };
+
+        return authenticatedUser;
+    }
+}
