7ac3acb7b1c97603116d76a01fa0da27cb070b8c
John Kristan Torremocha <johnkristan01@gmail.com>
2024-11-09 10:15:08 +0800

    feat: changed the file name

--- client/src/types/auth.ts
+++ client/src/types/auth.ts
@@ -1,5 +1,10 @@
-export type RegisterInputs = {
-    fullname: string,
+export type RegistrationData = {
+    name: string,
+    email: string,
+    password: string
+}
+
+export type SignInCredentials = {
     email: string,
     password: string
 }
