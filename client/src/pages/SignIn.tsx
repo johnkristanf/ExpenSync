3bd2f49732eadca5e56cb81a0f0cc45b14b376b8
John Kristan Torremocha <johnkristan01@gmail.com>
2024-11-09 10:04:20 +0800

    feat: implemented the react query provider and its useMutation in auth

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -1,31 +1,27 @@
-import { registerUser } from "@/api/post/auth";
-import { classNames } from "@/components/className";
+import { signInUser } from "@/api/post/auth";
 import { Loader } from "@/components/loader";
-import SuccessDialog from "@/components/successDialog";
-import { RegistrationData } from "@/types/auth";
+import { SignInCredentials } from "@/types/auth";
 import { useMutation } from "@tanstack/react-query";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 
-function RegisterPage(){
+function SignInPage(){
 
     const navigate = useNavigate();
-    const { register, handleSubmit, formState: { errors }, reset } = useForm<RegistrationData>();
+    const { register, handleSubmit, formState: { errors }, reset } = useForm<SignInCredentials>();
 
     const mutation = useMutation({
-        mutationFn: registerUser,
+        mutationFn: signInUser,
         onSuccess: () => {
-            setTimeout(() => {
-                navigate('/signin');
-            }, 3000)
+            navigate('/dashboard');
         },
         onError: (error) => {
-            console.error(`Registration Error: ${error}`);
+            console.error(`Sign In Error: ${error}`);
         }
     })
 
 
-    const onSubmit: SubmitHandler<RegistrationData> = data => {
+    const onSubmit: SubmitHandler<SignInCredentials> = data => {
         console.log(data);
         mutation.mutate(data)
         reset();
@@ -35,14 +31,8 @@ function RegisterPage(){
         <>
             { mutation.isPending && <Loader /> }
 
-            { mutation.isSuccess && <SuccessDialog /> }
-
             <div className="flex justify-center items-center w-full h-screen bg-gray-200">
-                <div className={classNames(
-                    "bg-indigo-600 w-[40%] flex flex-col gap-3 items-center justify-center font-semibold",
-                    Object.keys(errors).length > 0 ? "h-[90%]": "h-[78%]"
-                )}>
-
+                <div className="bg-indigo-600 w-[40%] h-3/4 flex flex-col gap-3 items-center justify-center font-semibold">
                     <div className="flex items-center gap-2">
                         <img src="/img/expensync_logo.png" alt="ExpenSync Logo" width={90} />
                         <h1 className="text-6xl text-white">ExpenSync</h1>
@@ -50,35 +40,15 @@ function RegisterPage(){
                     <h1 className="text-gray-200 text-sm w-[80%] text-center">Track and manage your personal finances effortlessly  with our Expense Tracker, helping you stay on top of your spending and budget effectively.</h1>
                 </div>
 
-                <div className={classNames(
-                    "w-[40%] bg-white flex flex-col justify-center px-6 py-12 lg:px-8",
-                    Object.keys(errors).length > 0 ? "h-[90%]": "h-[78%]"
-                )}>
+                <div className="h-3/4 w-[40%] bg-white flex flex-col justify-center px-6 py-12 lg:px-8 ">
                     <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                         <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-                            Register an account
+                            Sign in to your account
                         </h2>
                     </div>
 
-                    <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm font-semibold">
+                    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                     <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-
-                        <div>
-                            <label htmlFor="fullname" className="block text-sm/6 font-medium text-gray-900">
-                                Name
-                            </label>
-                            <div className="mt-2">
-                                <input
-                                    {...register("name", { required: true })} 
-                                    type="text"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
-                                />
-                            </div>
-
-                            {errors.name && <span className="text-red-800">Name is required</span>}
-
-                        </div>
-
                         <div>
                             <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
                                 Email address
@@ -88,6 +58,10 @@ function RegisterPage(){
                                     {
                                         ...register("email", { 
                                             required: true,
+                                            pattern: {
+                                                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                                                message: "Please enter a valid email address",
+                                            },
                                         })
 
                                     } 
@@ -100,9 +74,11 @@ function RegisterPage(){
                                 errors.email?.type == "required" && <span className="text-red-800">Email is required</span>
                             }
 
-                        </div>
+                            {
+                                errors.email?.type == "pattern" && <span className="text-red-800">Invalid Email Address</span> 
+                            }
 
-                        <div>
+                        </div>
                             
                         <div className="flex items-center justify-between">
                             <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
@@ -120,7 +96,11 @@ function RegisterPage(){
                             <input
                                 type="password"
                                 {...register("password", {
-                                    required: true,                              
+                                    required: true,
+                                    minLength: {
+                                      value: 8,
+                                      message: "Password must be at least 8 characters long",
+                                    },
                                 })}
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6"
                             />
@@ -130,24 +110,21 @@ function RegisterPage(){
                             errors.password?.type == "required" && <span className="text-red-800">Password is required</span>
                         }
 
-
-                        </div>
-
                         <div>
-                        <button
-                            type="submit"
-                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
-                            Register
-                        </button>
+                            <button
+                                type="submit"
+                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Sign in
+                            </button>
                         </div>
                     </form>
 
-                    <p className=" mt-3 text-center text-sm/6 text-gray-500 font-semibold">
-                        Already have an account?{' '}
+                    <p className="mt-10 text-center text-sm/6 text-gray-500 font-semibold">
+                        Not a member?{' '}
                         {
-                            <Link to={"/signin"} className="text-indigo-600 hover:opacity-75">
-                                Sign In here
+                            <Link to={"/register"} className="text-indigo-600 hover:opacity-75">
+                                Register here
                             </Link>
                         }
                     </p>
@@ -159,4 +136,4 @@ function RegisterPage(){
     );
 }
 
-export default RegisterPage;
+export default SignInPage;
