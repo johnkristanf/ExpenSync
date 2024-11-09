858512e58388d6b0c76bd0ae646f1cd8f64b0f48
John Kristan Torremocha <johnkristan01@gmail.com>
2024-11-09 10:09:59 +0800

    feat: installed and applied cookie parser for authentication

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,11 +3,15 @@ import { AppModule } from './app.module';
 import serverlessExpress from '@codegenie/serverless-express';
 import { Callback, Context, Handler } from 'aws-lambda';
 
+import cookieParser from 'cookie-parser'; 
+
+
 let server: Handler;
 
 async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
-
+  
+  app.use(cookieParser());
   await app.listen(3000);
 
   const expressApp = app.getHttpAdapter().getInstance();
