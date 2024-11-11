05cae281127252248720d6cc6f830bacca31f717
John Kristan Torremocha <johnkristan01@gmail.com>
2024-11-11 17:16:11 +0800

    feat: displaying the get wallets data in the page

--- client/src/types/wallets.ts
+++ client/src/types/wallets.ts
@@ -4,3 +4,12 @@ export type AddWalletType = {
     name: string;
     amount: number
 }
+
+export type Wallet = {
+    id: number;
+    name: string;
+    amount: number;
+    icon: string;
+    user_id: number;
+    created_at: Date;
+}
