631f5cd962458d865389c2a8a9e5d3c3e6162ba6
John Kristan Torremocha <johnkristan01@gmail.com>
2024-11-11 17:18:32 +0800

    feat: moved the guards and filter exceptions in the common folder

--- server/src/wallet/wallet.controller.ts
+++ server/src/wallet/wallet.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, Logger, Post, Request, UseGuards } from '@nestjs/common';
 import { WalletService } from './wallet.service';
-import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { JwtAuthGuard } from 'src/common/guard/jwt-auth.guard';
 import { NewWalletDTO } from './dto/wallet';
 
 @Controller('wallet')
