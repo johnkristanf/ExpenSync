06b5b91cb69933b4268037784798a6db8dafb59a
John Kristan Torremocha <johnkristan01@gmail.com>
2024-11-13 15:12:06 +0800

    feat: added the zustand store and budget type

---
+++ client/src/store/store.ts
@@ -0,0 +1,23 @@
+import { Wallet } from '@/types/wallets';
+import { create } from 'zustand';
+
+type WalletStore = {
+    wallet: Wallet;
+    addWallet: (wallet: Wallet) => void;
+}
+
+const InitialState: Wallet = {
+    id: 0,
+    name: 'N/A',
+    amount: 0,
+    icon: 'wallet',
+    user_id: 0,
+    created_at: new Date(),
+}
+
+const useWalletStore = create<WalletStore>((set) => ({
+    wallet: InitialState,
+    addWallet: (wallet) => set({ wallet }),
+}))
+
+export default useWalletStore;
