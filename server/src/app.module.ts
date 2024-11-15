47bf89b32f2efd534d0000b0016822a0025ffbad
John Kristan Torremocha <johnkristan01@gmail.com>
2024-11-15 10:19:46 +0800

    feat: removed the manual adding of entities and put synchronize comment to not use it in production

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,25 +2,25 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
 import { WalletModule } from './wallet/wallet.module';
-import { Wallet } from './wallet/entities/wallet.entity';
 import { BudgetsModule } from './budgets/budgets.module';
-import { Budgets } from './budgets/entities/budgets.entity';
 import { TransactionModule } from './transaction/transaction.module';
-import { Transaction } from './transaction/entities/transaction.entities';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+
+    // synchronize: true shouldn't be used in production 
+    // - otherwise you can lose production data
+    
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true,
-      entities: [User, Wallet, Budgets, Transaction]
     }),
+
     AuthModule,
     UsersModule,
     WalletModule,
