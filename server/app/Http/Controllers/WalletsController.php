2aad5beb677360b5c694e5dee42d814c2d8bf226
John Kristan Torremocha <johnkristan01@gmail.com>
2024-11-27 11:28:30 +0800

    feat: changes backend framework from nest js to laravel

---
+++ server/app/Http/Controllers/WalletsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Wallets;
+use Illuminate\Http\Request;
+
+class WalletsController extends Controller
+{
+    
+    public function store(Request $request)
+    {
+        //
+    }
+
+    
+    
+    public function update(Request $request, Wallets $wallets)
+    {
+        //
+    }
+
+   
+    public function destroy(Wallets $wallets)
+    {
+        //
+    }
+}
