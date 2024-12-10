48ba065b09ddda4f6a1d69045b2416c530385e82
John Kristan Torremocha <johnkristan01@gmail.com>
2024-12-10 08:29:37 +0800

    feat: implement wallets crud methods

--- server/app/Http/Controllers/WalletsController.php
+++ server/app/Http/Controllers/WalletsController.php
@@ -2,27 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\WalletsStoreRequest;
+use App\Http\Requests\WalletsUpdateRequest;
 use App\Models\Wallets;
-use Illuminate\Http\Request;
+use Illuminate\Http\Client\Request;
 
 class WalletsController extends Controller
 {
     
-    public function store(Request $request)
+    public function index()
     {
-        //
+        return Wallets::all();
+    }
+
+    public function store(WalletsStoreRequest $request)
+    {
+        $data = $request->validated();
+        return response()->json(Wallets::create($data), 201);
     }
 
     
     
-    public function update(Request $request, Wallets $wallets)
+    public function update(WalletsUpdateRequest $request, string $wallet_id)
     {
-        //
+        $data = $request->validated();
+
+        $wallet = Wallets::findOrFail($wallet_id);
+
+        $updated = $wallet->update([
+            'name' => $data['name'],
+            'icon' => $data['icon'],
+            'amount' => $wallet->amount + $data['amount']
+        ]);
+
+        if (!$updated) {
+            return response()->json(['message' => 'Error Occured in updating wallet'], 500);
+        }
+
+        return response()->json(['message' => 'Wallet updated successfully'], 200);
+
+                       
     }
 
    
-    public function destroy(Wallets $wallets)
+    public function destroy(string $wallet_id)
     {
-        //
+        Wallets::findOrFail($wallet_id)->delete();
+        return response()->json(['message' => 'Wallet deleted successfully'], 200);
     }
 }
