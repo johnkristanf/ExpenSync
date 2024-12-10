eee139b42b19dcb83b971918d66359535daf8587
John Kristan Torremocha <johnkristan01@gmail.com>
2024-12-10 08:32:05 +0800

    feat: implemented authentication method register and login

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,12 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\WalletsController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
 
 
-Route::apiResource("wallets", WalletsController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource("wallets", WalletsController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
