289af19aa3874a59b2f273d9f6b4f8bd6da99ddb
John Kristan Torremocha <johnkristan01@gmail.com>
2024-12-11 13:03:21 +0800

    feat: implement the hasMany and belongs relationship model

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function wallets()
+    {
+        return $this->hasMany(Wallets::class);
+    }
 }
